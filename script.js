3de9ab9713363c9b4e2431fe834c2608882a725f
Morgan Reilly <morgan.reilly27@gmail.com>
2019-08-12 11:10:53 +0100

    Preparing the data for training

--- script.js
+++ script.js
@@ -74,3 +74,60 @@ function createModel() {
  */
 const model = createModel();
 tfvis.show.modelSummary({ name: 'Model Summary' }, model);
+
+/**
+ * Convert the input data to tensors that we can use for machine 
+ * learning. We will also do the important best practices of _shuffling_
+ * the data and _normalizing_ the data
+ * MPG on the y-axis.
+ */
+function convertToTensor(data) {
+    // Wrapping these calculations in a tidy will dispose any 
+    // intermediate tensors.
+
+    return tf.tidy(() => {
+        /**
+         * Step 1. Shuffle the data
+         * Randomize the order of the examples we will feed to the training algorithm.
+         */
+        tf.util.shuffle(data);
+
+        /** 
+         * Step 2. Convert data to Tensor
+         * Make two arrays, one for input examples (horsepower entries)
+         * another for the true output values (known as labels in machine learning)
+        */
+        const inputs = data.map(d => d.horsepower)
+        const labels = data.map(d => d.mpg);
+
+        /**
+         * Converting each array data into 2D tensor
+         */
+        const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
+        const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
+
+        /**
+         * Step 3. Normalize the data to the range 0 - 1 using min-max scaling
+         * This is important as the intervals of many machine models built with TensorFlow.js are desinged to
+         * work with numbers that are not too big. 
+         * Common ranges to normalize data include 0-1. -1 - 1
+        */
+        const inputMax = inputTensor.max();
+        const inputMin = inputTensor.min();
+        const labelMax = labelTensor.max();
+        const labelMin = labelTensor.min();
+
+        const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin));
+        const normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin));
+
+        return {
+            inputs: normalizedInputs,
+            labels: normalizedLabels,
+            // Return the min/max bounds so we can use them later.
+            inputMax,
+            inputMin,
+            labelMax,
+            labelMin,
+        }
+    });
+}
